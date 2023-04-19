a1caeff1937eae987b71c0ef57f646d8d2499480
Birkhoff <zhiqic2@illinois.edu>
2023-04-19 02:24:59 +0000

    rename task_id

--- examples/example.py
+++ examples/example.py
@@ -30,20 +30,20 @@ default_args = {
 }
 
 dag = DAG(
-    'Download and Parse IEX PCAPs',
+    'download-and-parse-iex-pcaps',
     default_args=default_args,
     description='Download and Parse IEX PCAP files',
     schedule_interval=timedelta(days=1),
 )
 
 t1 = BashOperator(
-    task_id='download pcap files of the day',
+    task_id='download-pcap-files-of-the-day',
     bash_command='python3 /usr/local/iexdownloaderparser/src/download_iex_pcaps.py --start-date $(date +%Y-%m-%d) --end-date $(date +%Y-%m-%d) --download-dir /usr/local/iexdownloaderparser/data',
     dag=dag,
 )
 
 t2 = BashOperator(
-    task_id='parse pcap files of the day',
+    task_id='parse-pcap-files-of-the-day',
     bash_command='gunzip -d -c /usr/local/iexdownloaderparser/data/*$(date +%Y%m%d)*DEEP*.gz | tcpdump -r - -w - -s 0 | python3 /usr/local/iexdownloaderparser/src/parse_iex_pcaps.py /dev/stdin --symbols ALL --trade-date $(date +%Y%m%d) --output-deep-books-too',
     dag=dag,
 )
