685414302de85931f88ffb1a162781b7a6af6378
Kaiyang-Chen <kaiyang-chen@sjtu.edu.cn>
2023-04-29 01:39:10 -0500

    fix: init scirts for worker and master

--- prefect/init_worker.sh
+++ prefect/init_worker.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-sudo yum -y install git gcc pciutils epel-release python-devel python-pip
+sudo apt-get update
+sudo apt -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+sudo curl -O https://www.python.org/ftp/python/3.9.15/Python-3.9.15.tar.xz
+sudo tar -xf Python-3.9.15.tar.xz
+cd Python-3.9.15/ 
+sudo ./configure --enable-optimizations
+sudo make altinstall
+sudo apt-get -y install build-essential vim git python3-pip gcc
+cd ..
+sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3.9 get-pip.py
+sudo rm /usr/bin/python
+sudo ln -s /usr/local/bin/python3.9 /usr/bin/python
 sudo pip install --upgrade pip
 sudo pip install -U prefect
 sudo prefect config set PREFECT_API_URL=http://192.168.50.101:4200/api
