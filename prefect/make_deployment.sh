b0c9b361d27b7b93a2774f1f7ef16a968b3d2ab3
Kaiyang-Chen <kaiyang-chen@sjtu.edu.cn>
2023-05-09 20:37:14 -0500

    fix: initialization scripts

--- prefect/make_deployment.sh
+++ prefect/make_deployment.sh
@@ -4,6 +4,4 @@ vagrant ssh worker1 -c "tmux new-session -s dask-worker -d 'sudo dask-worker tcp
 vagrant ssh worker2 -c "tmux new-session -s dask-worker -d 'sudo dask-worker tcp://192.168.50.101:8786'"
 vagrant ssh worker1 -c "tmux new-session -s prefect-agent -d 'sudo prefect agent start -p hft-workpool'"
 vagrant ssh worker2 -c "tmux new-session -s prefect-agent -d 'sudo prefect agent start -p hft-workpool'"
-vagrant ssh master -c "tmux new-session -s prefect-master -d 'sudo prefect deploy ../../workflow/example.py:api_flow \
-    -n my-first-deployment \
-    -p hft-workpool'"
+vagrant ssh master -c "tmux new-session -s prefect-master -d ' sudo prefect deploy /home/vagrant/workflow/hft.py:hft_flow -n my-first-deployment-1 -p hft-workpool'"
