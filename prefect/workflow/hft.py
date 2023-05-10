611fec731aef58b21a459a05a7d9bea00cb0b02a
Kaiyang-Chen <kaiyang-chen@sjtu.edu.cn>
2023-05-09 20:35:18 -0500

    feat: update a real workflow that compute parallelly on different worker node

--- prefect/workflow/hft.py
+++ prefect/workflow/hft.py
@@ -3,15 +3,48 @@ from prefect import flow, task
 from prefect.task_runners import SequentialTaskRunner
 import datetime
 from prefect_dask.task_runners import DaskTaskRunner
+import subprocess
+
+
+def download_iex_pcaps(date):
+    command = f"sudo python workflow/download_iex_pcaps.py --start-date {date} --end-date {date} --download-dir vagrant_data"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        print(f"Something went wrong: {stderr}")
+    else:
+        print(stdout)
 
 
 
+def parse_iex_pcaps(date):
+    formatted_date = date.replace("-", "")
+    command = f"zcat vagrant_data/DEEP/*{formatted_date}*.gz | tcpdump -r - -w - -s 0 | sudo python workflow/parse_iex_pcaps.py /dev/stdin --symbols SPY --trade-date {formatted_date} --output-deep-books-too"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        print(f"Something went wrong: {stderr}")
+    else:
+        print(stdout)
+
+
+
+def compute_candle_chart(date):
+    formatted_date = date.replace("-", "")
+    command = f"zcat vagrant_data/book_snapshots/{formatted_date}_trades.csv.gz | sudo python workflow/compute_candle_chart.py -i /dev/stdin -o vagrant_data/{formatted_date}_candle_chart.csv"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        print(f"Something went wrong: {stderr}")
+    else:
+        print(stdout)
 
 
 @task
-def write_values(values, filename):
-    with open(filename, 'w') as f:
-        f.write(str(values))
+def compute(date):
+    download_iex_pcaps(date)
+    parse_iex_pcaps(date)
+    compute_candle_chart(date)
 
 @task
 def copy_file(source_filename, target_filename):
@@ -19,20 +52,16 @@ def copy_file(source_filename, target_filename):
         with open(target_filename, 'w') as target_file:
             target_file.write(source_file.read())
 
-# @flow(flow_run_name="workflow-on-{date:%A}",
-#       task_runner=SequentialTaskRunner())
-# def hft_flow(date: datetime.datetime):
-#     write_values.submit(date,'/data/test1')
-#     copy_file.submit('/data/test1', '/data/test2')
 
 
 @flow(flow_run_name="My Main Flow on {date:%A}",
       description="An example flow for a tutorial.",
       task_runner=DaskTaskRunner(address="tcp://192.168.50.101:8786"))
-def api_flow(date: datetime.datetime):
-    write_values.submit(date,'/vagrant_data/test1')
-    copy_file.submit('/vagrant_data/test1', '/vagrant_data/test2')
+def hft_flow(dates):
+    for date in dates:
+        compute.submit(date)
 
 
-api_flow(date=datetime.datetime.utcnow())
+hft_flow(["2022-08-01", "2022-08-02"])
+
 
