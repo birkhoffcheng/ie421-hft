658222dbc28541e189107e6c43e17dc4e3d3b3ea
zhiqic2 <zhiqic2@illinois.edu>
2023-05-10 13:59:20 -0500

    use single quotes

--- airflow/init_master.sh
+++ airflow/init_master.sh
@@ -3,7 +3,7 @@
 pacman -Syu --noconfirm python-pip python-pytz python-tqdm python-requests tmux
 
 AIRFLOW_VERSION=2.6.0
-PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+PYTHON_VERSION="$(python --version | cut -d ' ' -f 2 | cut -d '.' -f 1-2)"
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
 
