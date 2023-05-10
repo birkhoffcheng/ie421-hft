adeb5a242e9062517f37881ee62132b88a79201b
Birkhoff <zhiqic2@illinois.edu>
2023-05-10 15:37:15 -0500

    add missing import

--- airflow/dags/iexdlparse.py
+++ airflow/dags/iexdlparse.py
@@ -1,5 +1,5 @@
-from datetime import timedelta
-
+import datetime
+import pendulum
 import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
@@ -13,7 +13,7 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': datetime.timedelta(minutes=5),
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
