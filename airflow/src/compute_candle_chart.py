bd61ee9f72505905c16c1b1d2745ed848cb6b15d
Birkhoff <zhiqic2@illinois.edu>
2023-05-10 19:57:04 -0500

    add volume calculation

--- airflow/src/compute_candle_chart.py
+++ airflow/src/compute_candle_chart.py
@@ -4,9 +4,10 @@ import argparse
 import csv
 
 OPEN = 0
-CLOSE = 1
-HIGH = 2
-LOW = 3
+HIGH = 1
+LOW = 2
+CLOSE = 3
+VOLUME = 4
 
 def compute_candle_chart(input_file, output_file):
 	stocks = {}
@@ -15,19 +16,22 @@ def compute_candle_chart(input_file, output_file):
 		header = next(reader)
 		symbol_index = header.index('SYMBOL')
 		price_index = header.index('PRICE')
+		size_index = header.index('SIZE')
 		for row in reader:
 			symbol = row[symbol_index]
 			price = float(row[price_index])
+			size = int(row[size_index])
 			if symbol not in stocks:
-				stocks[symbol] = [price, price, price, price]
+				stocks[symbol] = [price, price, price, price, size]
 			else:
-				stocks[symbol][CLOSE] = price
 				stocks[symbol][HIGH] = max(stocks[symbol][HIGH], price)
 				stocks[symbol][LOW] = min(stocks[symbol][LOW], price)
+				stocks[symbol][CLOSE] = price
+				stocks[symbol][VOLUME] += size
 
 	with open(output_file, 'w') as f:
 		writer = csv.writer(f)
-		writer.writerow(['SYMBOL', 'OPEN', 'CLOSE', 'HIGH', 'LOW'])
+		writer.writerow(['SYMBOL', 'OPEN', 'HIGH', 'LOW', 'CLOSE', 'VOLUME'])
 		for symbol in stocks:
 			writer.writerow([symbol] + stocks[symbol])
 
